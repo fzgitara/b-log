5aea3d48961b5228006bd2e73ec7530cdd0f1bd4
fzgitara <fachrizalgita@gmail.com>
2018-04-26 13:51:00 +0700

    release 0: register and login on server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+mongoose.connect(`mongodb://${process.env.dbuser}:${process.env.dbpass}@ds157639.mlab.com:57639/b-log`, (err => {
+  if(err){
+    console.log('failed connect')
+  } else {
+    console.log('successfully connect to database')
+  }
+}))
 
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +38,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ server/controllers/users.js
@@ -0,0 +1,43 @@
+const User = require('../models/users')
+const bcrypt = require('bcryptjs')
+const salt = bcrypt.genSaltSync(10)
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+  register (req, res) {
+    const hash = bcrypt.hashSync(req.body.password, salt)
+    const user = new User()
+    user.username = req.body.username
+    user.password = hash
+    const token = jwt.sign({_id: user._id, username: user.username}, process.env.SECRET)
+    user.save()
+    .then(data => {
+      res.status(201).json({
+        message: 'Register success',
+        data,
+        token
+      })
+    })
+  },
+  login (req, res) {
+    User.findOne({username: req.body.username})
+    .exec()
+    .then(user => {
+      if(user){
+        const check = bcrypt.compareSync(req.body.password, user.password)
+        const token = jwt.sign({_id: user._id, username: user.username}, process.env.SECRET)
+        if(check){
+          res.status(200).json({
+            message: 'Login success',
+            user,
+            token
+          })
+        } else {
+          res.status(409).json('Wrong password')
+        }
+      } else {
+        res.status(409).json('Email not found')
+      }
+    })
+  }
+}
